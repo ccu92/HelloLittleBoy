0bd3155fd8911a11cdc6a788abd7c99b6d986099
ccu92 <yenping.tsao@gmail.com>
2019-03-14 22:03:56 +0800

    LittleBoy

--- HelloLittleBoy/ViewController.swift
+++ HelloLittleBoy/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSlider: UISlider!
+    
+    @IBOutlet weak var BlueSlider: UISlider!
+    
+    @IBOutlet weak var GreenSlider: UISlider!
+    
+    @IBOutlet weak var alphaSlider: UISlider!
+    @IBOutlet weak var LittleBoyImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    @IBAction func colorChangeSlider(_ sender: UISlider) {
+        LittleBoyImageView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(GreenSlider.value), blue:CGFloat(BlueSlider.value), alpha:CGFloat(alphaSlider.value))
+    }
+    
 
 }
 
